80aa3b73718a128e323a9667a03986b72209b2ae
DiegoPonce09 <ponce_diego09@hotmail.com>
2021-09-10 12:49:25 -0600

    Guia1

---
+++ Persona/Persona/Controllers/PersonaController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Persona.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Persona(string dui, string nombre, string apellido, string fecha, string direccion, string correo)
+        {
+            ViewBag.DUI = dui;
+            ViewBag.Nombre = nombre;
+            ViewBag.Apellido = apellido;
+            ViewBag.Fecha = fecha;
+            ViewBag.Direccion = direccion;
+            ViewBag.Correo = correo;
+            return View();
+        }
+    }
+}
---
+++ Persona/Persona/Models/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Persona.Models
+{
+    public class Persona
+    {
+        public int DUI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Direccion { get; set; }
+        public string Correo { get; set; }
+    }
+
+    public class PeliculaDBContext : DbContext
+    {
+        public DbSet<Persona> Personas { get; set; }
+    }
+
+}
